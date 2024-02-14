88e1aa1f5b680ea0fca4207f4e82add4c951d0a7
Aurélien <cadiou.aurelien@gmail.com>
2024-02-14 18:33:33 +0100

    Ajout des fichiers

---
+++ _data/data.js
@@ -0,0 +1,3 @@
+module.exports = {
+    footer_content_description: `Médiation logement est portée par La Fabrique Numérique de l'Ecologie (MTE-MCT). Ce service numérique est sponsorisé par Direction générale de l'Aménagement, du Logement et de la Nature , Ministère de la Transition Écologique.`
+}
---
+++ _data/metadata.js
@@ -0,0 +1,29 @@
+module.exports = {
+    title: "Médiation Logement",
+    url: "https://mediationlogement.beta.gouv.fr/",
+    newsletter: [
+        {
+            url: "",
+            title: "",
+            description: ""
+        }
+    ],
+    facebook_url: "",
+    mastodon_url: "",
+    twitter_url: "",
+    instagram_url: "",
+    linkedin_url: "",
+    youtube_url: "",
+    peertube_url: "",
+    github_url: "",
+    sourcehut_url: "",
+    repository_url: "",
+    language: "fr",
+    description: "Résoudre les litiges locatifs entre locataires et bailleurs par la médiation",
+    author: {
+        name: "Médiation Logement",
+        email: "contact@mediationlogement.beta.gouv.fr",
+        url: "https://mediationlogement.beta.gouv.fr/"
+    },
+    service: "beta.gouv.fr"
+}
